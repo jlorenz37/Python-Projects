38bef756d31a9d8aeaed542478868392a163dc12
jlorenz37 <j37.lorenz@gmail.com>
2023-01-26 17:59:27 -0600

    added imageio library and code to read in an image

--- image_inverter.py
+++ image_inverter.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+# import library
+import imageio.v2 as imageio
+
+# read an image
+image = imageio.imread(r"C:\Users\j37lo\OneDrive\Desktop\Important Documents\pumpkin.png")
+print(image.shape)
