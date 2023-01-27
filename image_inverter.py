462eb1df5a9457c0be3ab4b81ab2a3a597b88574
jlorenz37 <j37.lorenz@gmail.com>
2023-01-26 18:33:42 -0600

    Inverted a pumpkin image

--- image_inverter.py
+++ image_inverter.py
@@ -4,3 +4,6 @@ import imageio.v2 as imageio
 # read an image
 image = imageio.imread(r"C:\Users\j37lo\OneDrive\Desktop\Important Documents\pumpkin.png")
 print(image.shape)
+image = image.astype('float32')
+image = 255 - image
+imageio.imwrite(r"C:\Users\j37lo\OneDrive\Desktop\Important Documents\inverted_pumpkin.png", image)
